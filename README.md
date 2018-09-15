64ec0247f5bd37b5a65d24ee1360dc70274d7005
apleek3 <apleek3@gmail.com>
2018-09-14 19:01:32 -0700

    reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# burger
-A "Hamburger" logger with MySQL, Node, Express, Handlebars and a homemade ORM. Per assignment: needs to follow the MVC design pattern, use Node and MySQL to query and route data in the app, and Handlebars to generate the HTML.
--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (name) VALUES ('Buffalo Bleu');
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name, eaten) VALUES ('Manwich', true);
-INSERT INTO burgers (name, eaten) VALUES ('Soylent Green', true);
-INSERT INTO burgers (name, eaten) VALUES ('Turduckin', true);
-INSERT INTO burgers (name) VALUES ('Lamb');
